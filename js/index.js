b6eb1cce230f70031c83d56f24637d48d8c6c91b
ann7205 <elizabeth72052001@gmail.com>
2020-09-18 08:56:29 +1000

    Add files for task-7

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
-// console.log('this page')
-// const inputName = document.querySelector('#name')
-// const status = document.querySelector('.status')
-// const date = document.querySelector('#date')
-// const taskTitle = document.querySelector('#task-title')
-// const taskContent = document.querySelector('#task-content')
-
-let testTaskManager = new TaskManager()
+const inputName = document.querySelector('#name')
+const statusBtn = document.querySelector('.status-dropdown')
+const assignBtn = document.querySelector('.assign-dropdown')
+const date = document.querySelector('#date')
+const taskTitle = document.querySelector('#task-title')
+const taskContent = document.querySelector('#task-content')
+const form = document.querySelector('#form')
+const submitBtn = document.querySelector('.sub-btn')
+const cardsContainer = document.querySelector('.cards-container')
 
 // Name validation
 inputName.addEventListener('input', (e) => {
@@ -51,27 +52,50 @@ taskContent.addEventListener('input', (e) => {
     e.target.classList.remove('is-invalid', 'is-valid')
     let inputValue = e.target.value.trim()
     let inputLength = inputValue.length
-    if (inputLength === 0 || inputLength > 100) {
+    if (inputLength === 0 || inputLength > 50) {
         e.target.classList.add('is-invalid')
     } else {
         e.target.classList.add('is-valid')
     }
 })
 
+// Status / assignedTo validation
+let assignTo = ''
+assignBtn.addEventListener('click', (e) => {
+    assignTo = e.target.textContent
+})
+
+// Submit form
 function submitForm(e) {
     e.preventDefault()
-    document.querySelectorAll('input').forEach(item => item.classList.contains('is-valid'))
-    if (taskContent.classList.contains('is-valid')) {
+
+    let hasInvalid = false
+    document.querySelectorAll('input').forEach(item => {
+        if (!item.classList.contains('is-valid')) {
+            hasInvalid = true;
+        }
+    })
+
+    if (!hasInvalid && taskContent.classList.contains('is-valid')) {
         let nameVal = inputName.value
         let dateVal = date.value
+        let assignVal = assignTo
         let titleVal = taskTitle.value
         let contentVal = taskContent.value
-        testTaskManager.addTask(nameVal, dateVal, titleVal, contentVal)
+
+        testTaskManager.addTask(nameVal, assignVal, dateVal, titleVal, contentVal)
+
+        displayCards(testTaskManager.tasks)
+
+        inputName.value = ''
+        date.value = ''
+        assignTo = ''
+        taskTitle.value = ''
+        taskContent.value = ''
     } else {
-        console.log('the form is invalid')
+        alert('Please complete the form')
+        e.preventDefault()
+        e.stopPropagation()
     }
 }
-
-
-
 submitBtn.addEventListener('click', submitForm)
--- js/task-manager.js
+++ js/task-manager.js
@@ -1,13 +1,55 @@
 // console.log('this page')
-const inputName = document.querySelector('#name')
-const status = document.querySelector('.status')
-const date = document.querySelector('#date')
-const taskTitle = document.querySelector('#task-title')
-const taskContent = document.querySelector('#task-content')
-const form = document.querySelector('#form')
-const submitBtn = document.querySelector('.sub-btn')
-// const inputElem = document.querySelector('input')
 
+// display cards
+let displayCards = (tasksList) => {
+    let htmlContents = ''
+    tasksList.forEach(task => {
+        htmlContents += `
+            <div class="card">
+                <div class="card-body"> 
+                    <!-- status button  -->
+                    <div class="dropdown status-btn">
+                        <button class="btn" type="button" id="statusMenu" data-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false" title="status">
+                            <i class="far fa-check-square"></i>
+                        </button>
+                        <p class="due-date">Due date: <span class="date">${task.dueDate}</span><span>${task.name}</span></p>
+                        <div class="dropdown-menu" aria-labelledby="statusMenu">
+                            <a class="dropdown-item" href="#">Todo</a>
+                            <a class="dropdown-item" href="#">Progress</a>
+                            <a class="dropdown-item" href="#">Review</a>
+                            <a class="dropdown-item" href="#">Done</a>
+                        </div>
+                        <!-- select button  -->
+                        <button class="btn s-circle" type="button" value="Input"><i class="far fa-circle"></i></button>
+                    </div>
+                    <!-- assign button  -->
+                    <div class="dropdown assign-btn">
+                        <button class="btn" type="button" id="assignMenu" data-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false" title="assign to">
+                            <i class="fas fa-user-tag"></i>
+                        </button>
+                        <p class="task-title">${task.title}</p>
+                        <div class="dropdown-menu" aria-labelledby="assignMenu">
+                            <a class="dropdown-item" href="#">${task.assignedTo}</a>
+                        </div>
+                        <!-- task description button -->
+                        <button class="btn task-des-btn" type="button" data-toggle="collapse" data-target="#task-des1"
+                            aria-expanded="false" aria-controls="task-des1">
+                            <i class="fas fa-angle-double-down"></i>
+                        </button>
+                    </div>
+                    <div class="collapse" id="task-des1">
+                        <div class="card card-body">
+                            ${task.description}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
+    })
+    cardsContainer.innerHTML = htmlContents
+}
 
 // *make id number the same as array index number
 class TaskManager {
@@ -17,7 +59,32 @@ class TaskManager {
         this.currentID = id || 0
         // this.currentID = id === undefined ? 0 : id
     }
-    // // get tasks
+
+    // add task
+    addTask(name, assignedTo, dueDate, title, description) {
+        const task = {
+            id: this.currentID,
+            name: name,
+            status: 'Todo',
+            assignedTo: assignedTo,
+            dueDate: dueDate,
+            title: title,
+            description: description
+        }
+        this.tasks.push(task)
+        this.currentID++;
+    }
+
+    // render() {
+    //     let tasksHtmlList = []
+    //     this.tasks.forEach(task => {
+    //         let taskHtml = createTaskHtml(task.dueDate, task.title, task.description)
+    //         console.log(taskHtml)
+    //         tasksHtmlList.push(taskHtml)
+    //     })
+    //     let tasksHtml = tasksHtmlList 
+    // }
+    // get tasks
     // getAllTasks() {
     //     return this.tasks
     // }
@@ -27,21 +94,6 @@ class TaskManager {
     //         return task.status === status
     //     })
     // }
-
-    // add task
-    addTask(name, dueDate, title, description) {
-        let task = {
-            id: this.currentID,
-            name: name,
-            status: 'TODO',
-            // assignedTo: assignedTo,
-            dueDate: dueDate,
-            title: title,
-            description: description
-        }
-        this.tasks.push(task)
-        this.currentID++;
-    }
 
     // // delete task
     // deleteTask(task) {
@@ -59,3 +111,8 @@ class TaskManager {
     //     this.tasks[taskIndex].assignee = assignee
     // }
 }
+
+
+let testTaskManager = new TaskManager()
+let tasksList = testTaskManager.tasks
+displayCards(tasksList)
