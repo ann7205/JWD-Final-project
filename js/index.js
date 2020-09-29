8d570d6b3398d0a965d880749143bccf98494e5a
ann7205 <elizabeth72052001@gmail.com>
2020-09-29 22:16:00 +1000

    Update-Add delete function

--- js/index.js
+++ js/index.js
@@ -6,21 +6,9 @@ const taskTitle = document.querySelector('#task-title')
 const taskContent = document.querySelector('#task-content')
 const form = document.querySelector('#modal-form')
 const submitBtn = document.querySelector('.sub-btn')
-const cardsContainer = document.querySelector('.cards-container')
-
-// // Name validation
-// inputName.addEventListener('input', (e) => {
-//     e.target.classList.remove('is-invalid', 'is-valid')
-//     let inputValue = e.target.value.trim()
-//     let inputLength = inputValue.length
-//     // let invalidFeedback = inputLength === 0 ? e.target.classList.add('invalid') :
-//     //     (inputLength > 15 ? e.target.classList.add('is-invalid'))
-//     if (inputLength === 0 || inputLength > 20) {
-//         e.target.classList.add('is-invalid')
-//     } else {
-//         e.target.classList.add('is-valid')
-//     }
-// })
+const cardsContainer = document.querySelector('#cards-container')
+const binBtn = document.querySelector('.delete-list')
+const card = document.querySelector('.card')
 
 // Task title validation
 taskTitle.addEventListener('input', (e) => {
@@ -47,18 +35,6 @@ date.addEventListener('change', (e) => {
     }
 })
 
-// Task description validation
-// taskContent.addEventListener('input', (e) => {
-//     e.target.classList.remove('is-invalid', 'is-valid')
-//     let inputValue = e.target.value.trim()
-//     let inputLength = inputValue.length
-//     if (inputLength === 0 || inputLength > 50) {
-//         e.target.classList.add('is-invalid')
-//     } else {
-//         e.target.classList.add('is-valid')
-//     }
-// })
-
 // AssignedTo 
 let assignTo = ''
 assignBtn.addEventListener('click', (e) => {
@@ -70,6 +46,8 @@ let status = ''
 statusBtn.addEventListener('click', (e) => {
     status = e.target.textContent
 })
+
+
 
 // Submit form
 function submitForm(e) {
@@ -83,18 +61,21 @@ function submitForm(e) {
     })
     // if it's true as well as task content
     if (allValid) {
-
         let dateVal = date.value
         let statusVal = status
         let assignVal = assignTo
         let titleVal = taskTitle.value
         let contentVal = taskContent.value
-        // store data and assign to function as object
-        testTaskManager.addTask(statusVal || 'TODO', assignVal, dateVal, titleVal, contentVal)
-        // render the card from task's object
-        displayCards(tasksList)
 
-        // remove data
+        // store data and assign to function as object
+        newTaskManager.addTask(statusVal || 'To do', assignVal, dateVal, titleVal, contentVal)
+        // update the card
+        // save the cards to local storage
+        newTaskManager.save()
+        // render the cards from task's object
+        displayCards(newTaskManager.tasks, newTaskManager.selectedTasks)
+
+        // remove data from the form
         date.value = ''
         status = ''
         assignTo = ''
@@ -104,11 +85,65 @@ function submitForm(e) {
         document.querySelectorAll('input').forEach(item => {
             item.classList.remove('is-valid')
         })
-
     } else {
         alert('Please complete the form')
         e.preventDefault()
         e.stopPropagation()
     }
+
 }
 submitBtn.addEventListener('click', submitForm)
+
+function selectItem(e) {
+    if (e.target.matches('.fa-circle')) {
+        //  binBtn.classList.toggle('toggle-delete-btn')
+
+        let getId = e.target.parentElement.parentElement.parentElement.parentElement.dataset.taskId
+
+        newTaskManager.selectTask(getId)
+
+        if (newTaskManager.selectedTasks.length > 0) {
+            binBtn.classList.remove('toggle-delete-btn')
+        } else {
+            binBtn.classList.add('toggle-delete-btn')
+        }
+    }
+}
+cardsContainer.addEventListener('click', selectItem)
+
+binBtn.addEventListener('click', (e) => {
+    console.log(`deleting selected tasks`)
+    console.log(newTaskManager.selectedTasks)
+
+    newTaskManager.selectedTasks.forEach(taskId => {
+        newTaskManager.deleteTask(taskId)
+    })
+
+    displayCards(newTaskManager.tasks, newTaskManager.selectedTasks)
+})
+
+// Task description validation
+// taskContent.addEventListener('input', (e) => {
+//     e.target.classList.remove('is-invalid', 'is-valid')
+//     let inputValue = e.target.value.trim()
+//     let inputLength = inputValue.length
+//     if (inputLength === 0 || inputLength > 50) {
+//         e.target.classList.add('is-invalid')
+//     } else {
+//         e.target.classList.add('is-valid')
+//     }
+// })
+
+// // Name validation
+// inputName.addEventListener('input', (e) => {
+//     e.target.classList.remove('is-invalid', 'is-valid')
+//     let inputValue = e.target.value.trim()
+//     let inputLength = inputValue.length
+//     // let invalidFeedback = inputLength === 0 ? e.target.classList.add('invalid') :
+//     //     (inputLength > 15 ? e.target.classList.add('is-invalid'))
+//     if (inputLength === 0 || inputLength > 20) {
+//         e.target.classList.add('is-invalid')
+//     } else {
+//         e.target.classList.add('is-valid')
+//     }
+// })
--- js/task-manager.js
+++ js/task-manager.js
@@ -1,24 +1,59 @@
 // console.log('this page')
 
+// status object
+const statusMap = {
+    'To do': {
+        className: 'status1',
+        icon: 'far fa-circle'
+    },
+    'Progress': {
+        className: 'status2',
+        icon: 'fas fa-circle'
+    },
+    'Review': {
+        className: 'status3',
+        icon: 'fas fa-pause-circle'
+    },
+    'Done': {
+        className: 'status4',
+        icon: 'fas fa-check-circle'
+    }
+}
+
 // display cards
-let displayCards = (tasksList) => {
+let displayCards = (tasksList, selectedTasks) => {
+    console.log(`Displaying cards, selected ${selectedTasks}`)
+
     let htmlContents = ''
     tasksList.forEach(task => {
+        // console.log(task)
+        const isSelected = selectedTasks.includes(`${task.id}`)
+        // console.log(`task selected ${isSelected}`)
+        const selectedClass = isSelected ? 'selected' : ''
+
         htmlContents += `
-            <div class="card" data-task-id="${task.id}">
+            <div class="card shadow rounded ${statusMap[task.status.trim()].className} ${selectedClass}" data-task-id="${task.id}">
                 <div class="card-body"> 
                     <!-- status button  -->
                     <div class="dropdown status-btn">
                         <button class="btn" type="button" id="statusMenu" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false" title="status">
-                            <i class="far fa-check-square"></i>
+                            aria-expanded ="false"
+                            title="${task.status}">
+                            <i class="${statusMap[task.status.trim()].icon}"></i>
                         </button>
                         <p class="due-date">Due date: <span class="date">${task.dueDate}</span></p>
                         <div class="dropdown-menu" aria-labelledby="statusMenu">
-                            <a class="dropdown-item" href="#">${task.status}</a>
+                            <a class="status1 dropdown-item" href="#" name="To do"><i
+                                                class="far fa-circle"></i> <span>To do</span></a>
+                                        <a class="status2 dropdown-item" href="#" name="Progress"><i
+                                                class="fas fa-circle"></i> <span>Progress</span></a>
+                                        <a class="status3 dropdown-item" href="#" name="Review"><i
+                                                class="fas fa-pause-circle"></i> <span>Review</span></a>
+                                        <a class="status4 dropdown-item" href="#" name="Done"><i
+                                                class="fas fa-check-circle"></i> <span>Done</span></a>
                         </div>
                         <!-- select button  -->
-                        <button class="btn s-circle" type="button" value="Input"><i class="far fa-circle"></i></button>
+                        <button class="btn select-btn" type="button" value="Input"><i class="far fa-circle"></i></button>
                     </div>
                     <!-- assign button  -->
                     <div class="dropdown assign-btn">
@@ -50,19 +85,53 @@ let displayCards = (tasksList) => {
     cardsContainer.innerHTML = htmlContents
 }
 
+const testTasks = [{
+    id: 20,
+    // name: name,
+    status: 'To do',
+    assignedTo: 'Liz',
+    dueDate: '2020-10-10',
+    title: 'Test todo task',
+    description: 'Some description'
+}, {
+    id: 21,
+    // name: name,
+    status: 'Progress',
+    assignedTo: 'Liz',
+    dueDate: '2020-10-10',
+    title: 'Test progress task',
+    description: 'Some description'
+}, {
+    id: 22,
+    // name: name,
+    status: 'Review',
+    assignedTo: 'Liz',
+    dueDate: '2020-10-10',
+    title: 'Test review task',
+    description: 'Some description'
+}, {
+    id: 23,
+    // name: name,
+    status: 'Done',
+    assignedTo: 'Liz',
+    dueDate: '2020-10-10',
+    title: 'Test done task',
+    description: 'Some description'
+}]
 
 
 // *make id number the same as array index number
 class TaskManager {
     // define a tasks array/object to hold the tasks
-    constructor(id) {
-        this.tasks = []
-        this.currentID = id || 0
-        // this.currentID = id === undefined ? 0 : id
+    constructor(newId) {
+        this.tasks = [] //testTasks 
+        this.selectedTasks = []
+        this.currentID = newId || 0
+        //Create a new array without the elements we want removed included.
     }
 
-    // add task
     addTask(status, assignedTo, dueDate, title, description) {
+
         const task = {
             id: this.currentID,
             // name: name,
@@ -71,46 +140,109 @@ class TaskManager {
             dueDate: dueDate,
             title: title,
             description: description
+
         }
         this.tasks.push(task)
         this.currentID++;
     }
+    // save tasks to local storage
+    save() {
+        let tasksJson = JSON.stringify(this.tasks)
+        let idJson = JSON.stringify(this.currentID)
+        localStorage.setItem('tasks', tasksJson)
+        localStorage.setItem('id', idJson)
+        // console.log('run save')
+    }
 
-    // render() {
-    //     let tasksHtmlList = []
-    //     this.tasks.forEach(task => {
-    //         let taskHtml = createTaskHtml(task.dueDate, task.title, task.description)
-    //         console.log(taskHtml)
-    //         tasksHtmlList.push(taskHtml)
-    //     })
-    //     let tasksHtml = tasksHtmlList 
-    // }
-    // get tasks
-    // getAllTasks() {
-    //     return this.tasks
-    // }
-    // // get tasks with a given status: when requiring for certain status and list the tasks
-    // getTasksWithStatus(status) {
-    //     return this.tasks.filter(function (task) {
-    //         return task.status === status
-    //     })
-    // }
+    // load data from local storage
+    load() {
+        let getTasks = localStorage.getItem('tasks')
+        let tasksParse = JSON.parse(getTasks)
+        this.tasks = tasksParse
+        let getId = localStorage.getItem('id')
+        let idParse = JSON.parse(getId)
+        this.currentID = idParse
+        // console.log(this.currentID)
+        displayCards(this.tasks, this.selectedTasks)
+    }
 
-    // // delete task
-    // deleteTask(task) {
-    //     let taskIndex = this.tasks.findIndex(t => t.id === task.id)
-    //     this.tasks.splice(taskIndex, 1)
-    // }
-    // // update task status
-    // updateTask(taskId, status) {
-    //     const taskIndex = this.tasks.findIndex(task => task.id === taskId)
-    //     this.task[taskIndex].status = status
-    // }
-    // // Assign a task to someone
-    // assignTask(taskId, assignee) {
-    //     const taskIndex = this.tasks.findIndex(task => task.id === taskId)
-    //     this.tasks[taskIndex].assignee = assignee
-    // }
+    selectTask(taskId) {
+        // console.log(`Toggle selection task ${taskId}`)
+        if (this.selectedTasks.includes(taskId)) {
+
+            // remove this task from a list of selected tasks
+            this.selectedTasks = this.selectedTasks.filter(id => id !== taskId)
+        } else {
+            this.selectedTasks.push(taskId)
+        }
+
+        console.log(this.selectedTasks)
+
+        displayCards(this.tasks, this.selectedTasks)
+    }
+
+    // delete task
+    deleteTask(taskId) {
+        console.log(taskId)
+        // tasksForDelete = []
+
+        // let taskIndex = this.tasks.findIndex(item => console.log(item.id)) //item.id === taskId)
+
+        // let sliceTask = this.tasks.slice(taskId, taskId + 1)
+        // console.log(sliceTask)
+        //  console.log(this.tasks)
+        // tasksForDelete.push(this.tasks[taskIndex])
+
+        this.tasks = this.tasks.filter(task => task.id != taskId)
+        this.save()
+    }
+
 }
-let testTaskManager = new TaskManager()
-let tasksList = testTaskManager.tasks
+/*
+const original = [1, 2, 3]
+const removed = original.slice(1, 2)
+console.log(`1 item was removed, original size ${original.length}, new size: ${removed.length}`)
+*/
+
+let newTaskManager = new TaskManager()
+newTaskManager.load()
+let tasksList = newTaskManager.tasks
+let listCards = displayCards(tasksList, newTaskManager.selectedTasks)
+
+
+// cardsContainer.addEventListener('click', (e) => {
+//     console.log(e.target.children)
+// })
+
+// update task status
+// updateTask(taskId, status) {
+//     const taskIndex = this.tasks.findIndex(task => task.id === taskId)
+//     this.task[taskIndex].status = status
+// }
+
+// render() {
+//     let tasksHtmlList = []
+//     this.tasks.forEach(task => {
+//         let taskHtml = createTaskHtml(task.dueDate, task.title, task.description)
+//         console.log(taskHtml)
+//         tasksHtmlList.push(taskHtml)
+//     })
+//     let tasksHtml = tasksHtmlList 
+// }
+
+// get tasks
+// getAllTasks() {
+//     return this.tasks
+// }
+// // get tasks with a given status: when requiring for certain status and list the tasks
+// getTasksWithStatus(status) {
+//     return this.tasks.filter(function (task) {
+//         return task.status === status
+//     })
+// }
+
+//     // Assign a task to someone
+//     assignTask(taskId, assignee) {
+//         const taskIndex = this.tasks.findIndex(task => task.id === taskId)
+//         this.tasks[taskIndex].assignee = assignee
+//     }
